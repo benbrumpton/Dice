0bb1fa1d4c60e26a45916ce96d24dd1ee8de356f
benbrumpton <bmbrumpton@gmail.com>
2020-04-24 23:01:24 +0200

    Initial Commit

---
+++ Dice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Dice
+//
+//  Created by benb on 24/04/2020.
+//  Copyright © 2020 benb. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
